8b53fa94946219a1c1033363135a48c8aabffa93
Andrew Beauchamp <andrewbeauchamp@macbook-pro-55.local>
2018-10-09 09:52:10 -0500

    Initial Commit

---
+++ GuessingGameIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GuessingGameIOS
+//
+//  Created by Andrew Beauchamp on 10/9/18.
+//  Copyright © 2018 Andrew Beauchamp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
